89ad9936ca94b09a9f271430402ec90265f6bf43
Hoàng Mạnh Khiêm <khiemhm04@gmail.com>
2023-11-24 21:55:57 +0700

    upload

---
+++ seting.py
@@ -0,0 +1,12 @@
+try:
+    from math import ceil
+    import math
+    from svgpathtools import svg2paths2
+    import numpy as np
+    import webcolors
+except:
+    import os
+    os.system('pip install svgpathtools')
+    os.system('pip install numpy')
+    os.system('pip install numpy')
+    os.system('pip install webcolors')
